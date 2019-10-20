ace9c845d471fe5d49fb8ea082480a5bab5da6cb
katmai <katmai.mobil@gmail.com>
2019-10-21 01:24:13 +0200

    dd

--- convert_ui_files.sh
+++
@@ -1,9 +0,0 @@
-echo -e "\n"
-pyrcc5 ico/iconos.qrc -o iconos_rc.py
-for input_file in ui/*
-do
-    echo " Conviertiendo fichero '$input_file'..."
-    output_file=${input_file/.ui/.py}
-    pyuic5 -o src/$output_file $input_file
-done
-
