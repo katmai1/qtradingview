f3a929b31abf4c61f2eabfe075b53504759bb19d
katmai <katmai.mobil@gmail.com>
2020-06-21 19:57:26 +0200

    rebased and fix errors with imports

---
+++ app/base/mainwindow.py
@@ -0,0 +1,98 @@
+import logging
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QCoreApplication as qapp
+
+# from ui.mainwindow_Ui import Ui_MainWindow
+
+from app.markets.dock import DockMarkets
+from app.debug.dock import DockDebug, Qlogger
+
+from app.base.widgets import CustomWebEnginePage
+from app.base.dialog_config import DialogConfig
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType("ui/mainwindow.ui")
+
+
+# ─── MAIN WINDOW ────────────────────────────────────────────────────────────────
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
+    def __init__(self, ctx, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        #
+        self.html = None
+        self.ctx = ctx
+        self.config = ctx.config
+
+        # logs
+        log_mode = logging.INFO
+        if self.ctx.debug:
+            log_mode = logging.DEBUG
+        qlog = Qlogger(self)
+        logging.getLogger().addHandler(qlog)
+        logging.getLogger().setLevel(log_mode)
+
+        # webenginepage
+        page = CustomWebEnginePage(self.webview)
+        self.webview.setPage(page)
+
+        # signals
+        self._docks()
+        self._signals()
+
+        # carga market inicial
+        self.load_chart(self.config['initial_market'], self.config['initial_exchange'])
+
+    def _tr(self, contexto, mensaje):
+        return self.ctx.tr(contexto, mensaje)
+
+    # signal connectors
+    def _signals(self):
+        self.actionSettings.triggered.connect(self.openDialogSettings)
+        self.actionFull_Screen.toggled.connect(self.onActionFullScreen)
+        self.actionMarkets.toggled['bool'].connect(self.dock_markets.setVisible)
+        self.actionDebug.toggled['bool'].connect(self.dock_debug.setVisible)
+
+    def _docks(self):
+        self.setTabPosition(QtCore.Qt.AllDockWidgetAreas, QtWidgets.QTabWidget.North)
+        self.dock_markets = DockMarkets(self)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.dock_markets)
+        self.dock_debug = DockDebug(self)
+        self.addDockWidget(QtCore.Qt.BottomDockWidgetArea, self.dock_debug)
+
+    # ─── EVENTS ─────────────────────────────────────────────────────────────────────
+
+    def openDialogSettings(self):
+        dialog = DialogConfig(self)
+        dialog.load_config(self.config)
+        dialog.exec_()
+
+    # fullscreen
+    def onActionFullScreen(self):
+        if self.actionFull_Screen.isChecked():
+            self.showFullScreen()
+        else:
+            self.showNormal()
+            self.showMaximized()
+
+    # confirmacion de salida
+    def closeEvent(self, event):
+        result = QMessageBox.question(
+            self, self._tr("mainwindow", 'Exit'), self._tr("mainwindow", "Do you want quit?"),
+            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if result == QMessageBox.Yes:
+            self.ctx.app.quit()
+
+    # ────────────────────────────────────────────────────────────────────────────────
+
+    # carga un market en la pagina
+    def load_chart(self, market, exchange):
+        mar, ket = market.split("/")
+        url = f"https://es.tradingview.com/chart/?symbol={exchange.upper()}:{mar}{ket}"
+        self.webview.setUrl(QtCore.QUrl(url))
+        self.currentExchange = exchange
+        self.currentMarket = market
---
+++ app/markets/widgets.py
@@ -0,0 +1,145 @@
+import logging
+
+from PyQt5.QtWidgets import QStyledItemDelegate, QListWidgetItem, QStyleOptionViewItem, QMenu, QAction
+from PyQt5.QtGui import QIcon, QKeySequence
+
+from app.models.markets import Markets
+
+
+class CustomItemDelegate(QStyledItemDelegate):
+    def paint(self, painter, option, index):
+        option.decorationPosition = QStyleOptionViewItem.Right
+        super(CustomItemDelegate, self).paint(painter, option, index)
+
+
+class CustomContextMenu(QMenu):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.mw = self.parent().parent().parent().parent()  # ref a mainwindow
+
+    def handler(self, position):
+        self._item = self._get_item(position)
+        if self._item is not None:
+            self._insert_favorite(self._item)
+            self._insert_actions(self._item)
+            self.exec_(self.parent().mapToGlobal(position))
+
+    # ─── PRIVATE METHODS ────────────────────────────────────────────────────────────
+    # devuelve el item seleccionado o devuelve None si no lo hay
+    def _get_item(self, position):
+        try:
+            return self.parent().itemAt(position)
+        except Exception as e:
+            logging.warning("Exception trying get item on this position.")
+            logging.error(e.__str__())
+            return None
+
+    # funcion para configurar las opciones rapidamente
+    def _action(self, icono, texto, cmd, shortcut=None):
+        action = QAction(QIcon(icono), texto, self, triggered=cmd)
+        if shortcut is not None:
+            action.setShortcut(shortcut)
+        return action
+
+    # inserta los menus que ejecutan eventos
+    def _insert_actions(self, item):
+        self.addAction(self._action(":/actions/markets", "Load chart...", self._run_load_chart, "Return"))
+        self.addSeparator()
+        self.addAction(self._action(":/actions/settings", "Set as initial market", self._run_set_initial_market))
+
+    # inserta menu favorito, ejecuta evento externo
+    def _insert_favorite(self, item):
+        v = {}
+        v[True] = {"txt": "Remove from favorite", "ico": ":base/voidstar"}
+        v[False] = {"txt": "Add to favorite", "ico": ":base/star"}
+        self.addAction(self._action(v[item.is_favorite]['ico'], v[item.is_favorite]['txt'], item.toggle_favorite, "Esc"))
+
+    # ─── EVENTS ────────────────────────────────────────────────────────────────────
+
+    def _run_load_chart(self):
+        self.mw.load_chart(self._item.symbol, self._item.exchange)
+
+    def _run_set_initial_market(self):
+        self.mw.config['initial_exchange'] = self._item.exchange.title()
+        self.mw.config['initial_market'] = self._item.symbol
+        self.mw.ctx.save_config()
+
+# ────────────────────────────────────────────────────────────────────────────────
+
+# ─── ITEMS ──────────────────────────────────────────────────────────────────────
+
+
+class CustomItem(QListWidgetItem):
+
+    exchange = None
+
+    @property
+    def symbol(self):
+        return self.text()
+
+    @property
+    def is_favorite(self):
+        return Markets.check_symbol_is_fav(self.symbol, self.exchange)
+    
+    @property
+    def is_margin(self):
+        return Markets.check_symbol_is_margin(self.symbol, self.exchange)
+
+    def _get_icon(self):
+        if self.is_favorite:
+            return QIcon(":/base/star")
+        return QIcon(":/base/voidstar")
+
+    def _mostrar(self):
+        self.setHidden(False)
+
+    def _ocultar(self):
+        self.setHidden(True)
+
+    def configurar(self, symbol, exchange):
+        self.setText(symbol)
+        self.exchange = exchange.lower()
+        self.setIcon(self._get_icon())
+
+    def mostrar(self, lista_mode, filtro):
+        if lista_mode == "all":
+            self.mostrar_all(True, filtro)
+        elif lista_mode == "fav":
+            self.mostrar_fav(True, filtro)
+        elif lista_mode == "margin":
+            self.mostrar_margin(True, filtro)
+
+    #
+    def mostrar_all(self, all_actived, filtro):
+        if all_actived:
+            if filtro.upper() in self.symbol:
+                self._mostrar()
+            else:
+                self._ocultar()
+
+    def mostrar_fav(self, fav_actived, filtro):
+        if fav_actived:
+            if self.is_favorite:
+                if filtro.upper() in self.symbol:
+                    self._mostrar()
+                else:
+                    self._ocultar()
+            else:
+                self._ocultar()
+
+    def mostrar_margin(self, margin_actived, filtro):
+        if margin_actived:
+            if self.is_margin:
+                self._mostrar()
+            else:
+                self._ocultar()
+        else:
+            self._mostrar()
+
+    def toggle_favorite(self):
+        item = Markets.get_symbol_by_exchange(self.text(), self.exchange)
+        item.toggle_fav()
+        self.setIcon(self._get_icon())
+
+# ────────────────────────────────────────────────────────────────────────────────
