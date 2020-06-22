b9392faddae13abb6e4d4c88ef09e3dc1307abe9
katmai <katmai.mobil@gmail.com>
2020-06-22 09:09:56 +0200

    update i18n and icons

--- app/i18n/en_EN.ts
+++ app/i18n/en_EN.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="en" sourcelanguage="es">
+<!DOCTYPE TS><TS version="2.0" language="en" sourcelanguage="es">
 <context>
     <name>DialogConfig</name>
     <message>
--- app/i18n/es_ES.ts
+++ app/i18n/es_ES.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="es_ES" sourcelanguage="es">
+<!DOCTYPE TS><TS version="2.0" language="es_ES" sourcelanguage="es">
 <context>
     <name>DialogConfig</name>
     <message>
